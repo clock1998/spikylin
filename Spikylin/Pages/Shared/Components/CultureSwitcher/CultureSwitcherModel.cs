c262ac8cd5ab7547b01a28186ce2ff7cc4f4c6c6
linyi <jianglinyi97@gmail.com>
2025-05-20 23:42:14 -0400

    add language switcher

--- Spikylin/Pages/Shared/Components/CultureSwitcher/CultureSwitcherModel.cs
+++ Spikylin/Pages/Shared/Components/CultureSwitcher/CultureSwitcherModel.cs
@@ -4,7 +4,7 @@ namespace Spikylin.Pages.Shared.Components.CultureSwitcher
 {
     public class CultureSwitcherModel
     {
-        public CultureInfo CurrentUICulture { get; set; }
-        public List<CultureInfo> SupportedCultures { get; set; }
+        public CultureInfo CurrentUICulture { get; set; } = null!;
+        public List<CultureInfo> SupportedCultures { get; set; } = null!;
     }
 }

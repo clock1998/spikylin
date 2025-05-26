ddfccd4a7d7e05273f5aed39b911a4f8a8e31f4a
linyi <jianglinyi97@gmail.com>
2025-05-25 23:36:23 -0400

    fix navbar

--- Spikylin/Pages/About/Index.cshtml.cs
+++ Spikylin/Pages/About/Index.cshtml.cs
@@ -6,16 +6,8 @@ namespace Spikylin.Pages.About;
 
 public class AboutModel(IWebHostEnvironment env) : PageModel
 {
-    public string Html { get; set; } = string.Empty;
     public void OnGet()
     {
-        // Use the fileName to locate and load the document.
-        var contentPath = Path.Combine(env.ContentRootPath, "Pages/About");
-        var filePath = CultureInfo.CurrentCulture.Name == "en" ? Path.Combine(contentPath,  "content.md") : Path.Combine(contentPath, "content.fr.md");
-        if (System.IO.File.Exists(filePath))
-        {
-            var markdownContent = System.IO.File.ReadAllText(filePath);
-            Html = MarkdigMarkdownParser.Parse(markdownContent).Html;
-        }
+
     }
 }
--- Spikylin/Pages/Index.cshtml.cs
+++ Spikylin/Pages/Index.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Spikylin.Infrastructure.Helper;
+using System.Globalization;
 
 namespace Spikylin.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel(IWebHostEnvironment env) : PageModel
     {
+        public string Html { get; set; } = string.Empty;
         public void OnGet()
         {
+            // Use the fileName to locate and load the document.
+            var contentPath = Path.Combine(env.ContentRootPath, "Pages");
+            var filePath = CultureInfo.CurrentCulture.Name == "en" ? Path.Combine(contentPath, "content.md") : Path.Combine(contentPath, "content.fr.md");
+            if (System.IO.File.Exists(filePath))
+            {
+                var markdownContent = System.IO.File.ReadAllText(filePath);
+                Html = MarkdigMarkdownParser.Parse(markdownContent).Html;
+            }
         }
     }
 }
